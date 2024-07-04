2f1cc124ce7f7205438141faa1ac50162695de69
vijayalakshmi <58000247+viar84@users.noreply.github.com>
2024-07-04 13:25:13 +0530

    Update Testing.java

--- SampleProject/src/main/java/Testing.java
+++ SampleProject/src/main/java/Testing.java
@@ -4,7 +4,7 @@ public class Testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome");
-//Branch 1 -modification --change in git
+//Branch 1 -modification
 	}
 
 }
